b8f56f99c1768c43046144fff64c917cfd477023
Pavlo Chudin <maks.chudin567@gmail.com>
2024-05-22 14:06:14 +0300

    Change style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,20 @@
-import styles from '../../App.module.scss';
+import '../../App.scss';
 
 export const Person = ({ person }) => {
   const partnerType = person.sex === 'm' ? 'wife' : 'husband';
 
   return (
-    <section className={styles.Person}>
-      <h2 className={styles.Person__name}>My name is {person.name}</h2>
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
       {person.age !== null && person.age !== undefined && (
-        <p className={styles.Person__age}>I am {person.age} years old</p>
+        <p className="Person__age">I am {person.age} years old</p>
       )}
       {person.isMarried ? (
-        <p className={styles.Person__partner}>
+        <p className="Person__partner">
           {person.partnerName} is my {partnerType}
         </p>
       ) : (
-        <p className={styles.Person__partner}>I am not married</p>
+        <p className="Person__partner">I am not married</p>
       )}
     </section>
   );
