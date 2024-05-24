9b5c7c243ce4b77bef17319e1dca61e353521091
Pavlo Chudin <maks.chudin567@gmail.com>
2024-05-24 12:58:57 +0300

    complet the task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,9 +6,10 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age !== null && person.age !== undefined && (
+      {person.age && (
         <p className="Person__age">I am {person.age}</p>
       )}
+
       {person.isMarried ? (
         <p className="Person__partner">
           {person.partnerName} is my {partnerType}
